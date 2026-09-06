5997a7ab31383dd11f6174ed9f75a52efd9cdfe3
pritambasak2005-dotcom <pritambasak2005@gmail.com>
2026-09-06 12:36:27 +0530

    Add new CORS origin for Netlify app

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ origins = [
    "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://pritambasak2005-dotcom.github.io",
+   "https://basakcut.netlify.app/",
 ]
 
 app.add_middleware(
